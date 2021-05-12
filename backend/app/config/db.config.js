673ba3986164924428f8e05242600bd4f2e38164
GuiFreire <guilherme.cezarino.freire@gmail.com>
2021-05-12 11:47:18 -0300

    Banco

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "Avanade1401!",
-    DB: 'db_projeto_pi',
+    PASSWORD: "Gn@291218",
+    DB: 'db_pi',
     dialect: "mysql",
     pool: {
         max: 5,
