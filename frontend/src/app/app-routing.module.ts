5effcb836fbdbb205ba3e831758ed0292397662a
GuiFreire <guilherme.cezarino.freire@gmail.com>
2021-05-11 23:05:14 -0300

    Criar component Contato

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/template/login/login.component';
+import { ContatoComponent } from './components/template/contato/contato.component';
 
 const routes: Routes = [{
   path: 'login',
   component: LoginComponent
-}];
+},
+{
+  path:'contato',
+  component: ContatoComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/template/login/login.component';
+import { ContatoComponent } from './components/template/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
