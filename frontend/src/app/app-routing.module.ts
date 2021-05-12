9ee82659f8e37b73b6071903558feae445cbc3fa
Lidia Kobayashi <lidia.kobayashi@gmail.com>
2021-05-12 11:13:44 -0300

    Ajuste pasta template

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { PerfilEditarComponent } from './components/perfil-editar/perfil-editar.component';
+import { PerfilEditarComponent } from './components/template/perfil-editar/perfil-editar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'perfis', pathMatch: 'full' },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PerfilEditarComponent } from './components/perfil-editar/perfil-editar.component';
+import { PerfilEditarComponent } from './components/template/perfil-editar/perfil-editar.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
